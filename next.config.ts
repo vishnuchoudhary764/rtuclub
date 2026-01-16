b7542893c05dd0bca34ffbcff72340938528c6d1
vishnuchoudhary764 <vasanaram162@gmail.com>
2026-01-17 03:48:35 +0530

    jan2026th

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-
-   eslint: {
-    ignoreDuringBuilds: true, // skips linting when building
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
